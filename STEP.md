b281ecaf8fb03b099c0cade903f984d6154b67f4
tanqingqing <tanqingqing@hualala.com>
2020-04-08 14:45:24 +0800

    react.createElement

--- STEP.md
+++ STEP.md
@@ -2,14 +2,18 @@
 2、使用了react的新特性hooks
 3、Contex的使用
 4、ref的理解
+11、webpack
 ********待做**********
 5、高阶组件
 6、react的节点渲染算法
+77、该项目中文件路径的引入问题
+
+<!-- 怎么感觉bable是专门为react生的呢~~ -->
 
 ********others************
 7、何为单页面应用（SPA----Single Page web Application）
 8、router的引入
 9、redux的使用
 10、引入antd
-11、webpack
 12、npm run build用来干嘛
+13 node学习
--- src/pages/refs/refsInClass.js
+++ src/pages/refs/refsInClass.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 export default class RefsInClass extends Component{
     constructor(props){
@@ -21,13 +22,20 @@ export default class RefsInClass extends Component{
 
     render(){
         return (
-            <div>
-                <p>refs in class</p>
-                <button onClick={() => this.getClassFuns('one')}>获取焦点1</button>
-                <button onClick={() => this.getClassFuns('two')}>获取焦点2</button>
-                <input onChange={()=>{}} ref={this.inputRef} />
-                <input onChange={()=>{}} ref={el => this.inputRef2 = el} />
-            </div>
+            React.createElement('div',{
+                key: '123',
+                ref:'tanqq'
+            },[
+                React.createElement('p',null, 'ssooososs'),
+                React.createElement('p',null, '6666666'),
+            ])
+            // <div>
+            //     <p>refs in class</p>
+            //     <button onClick={() => this.getClassFuns('one')}>获取焦点1</button>
+            //     <button onClick={() => this.getClassFuns('two')}>获取焦点2</button>
+            //     <input onChange={()=>{}} ref={this.inputRef} />
+            //     <input onChange={()=>{}} ref={el => this.inputRef2 = el} />
+            // </div>
         )
     }
 }
