e85104ceca99eb01bcab6e495a523d55b3de4caa
tanqingqing <tanqingqing@hualala.com>
2020-05-19 10:59:29 +0800

    增加一个ES6代理Proxy的用例

--- src/App.js
+++ src/App.js
@@ -28,12 +28,52 @@ import Game from './pages/game/index';
         });
     }
 
+    getProxy = () => {
+        const alice =  {
+            birthday: '2002-01-01',
+            age: 20,
+            height: 180,
+            weight: 65,
+            sex: 'man'
+        }
+
+        const calculateAge = birthday => {
+            return '18';
+        }
+        const handler = {
+            get (target, key) {
+                if(key === 'age'){
+                    return calculateAge(new Date(target.birthday))
+                }
+                return target(key)
+            },
+            set (target, key, value) {
+                if(key === 'birthday'){
+                    throw new Error('Birthday is readonly')
+                }
+                return true;
+            },
+            deleteProperty (target, key) {
+                if(key in target){
+                    return false;
+                }
+                return delete target[key]
+            }
+        }
+        const proxy = new Proxy(alice, handler);
+        console.log("000000000",proxy)
+        proxy.age = 23;
+        delete proxy.weight;
+        console.log("111111111",proxy,proxy.age)
+    }
+
     render() {
         return (
             <div>
                 <div className="title-div">
                     <span className="gradient-h1">0、三子棋盘</span>
                 </div>
+                <button onClick={this.getProxy}>掉一下</button>
                 <Game />
                 <div className="title-div">
                     <span className="gradient-h1">1、hooks的使用</span>
